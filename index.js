6cb3154d57b9dc3cb63bf1551f27472117c55d67
Javier Retamal <javier.retamal.fontannaz@gmail.com>
2022-12-06 10:30:42 -0300

    Change currency
    Change currency from usd to clp

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ fetch("https://api.coingecko.com/api/v3/coins/shiba-inu")
             <span>${data.name}</span>
         `
         document.getElementById("crypto").innerHTML += `
-            <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
-            <p>👇: $${data.market_data.low_24h.usd}</p>
+            <p>🎯: $${data.market_data.current_price.clp}</p>
+            <p>👆: $${data.market_data.high_24h.clp}</p>
+            <p>👇: $${data.market_data.low_24h.clp}</p>
         `
     })
     .catch(err => console.error(err))
