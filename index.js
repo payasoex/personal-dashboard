738ba92f2c56e58810ead5e62f2458b566e26a97
Javier Retamal <javier.retamal.fontannaz@gmail.com>
2022-12-13 10:13:17 -0300

    change cryptocurrency to cardano

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
-fetch("https://api.coingecko.com/api/v3/coins/shiba-inu")
+fetch("https://api.coingecko.com/api/v3/coins/cardano")
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
